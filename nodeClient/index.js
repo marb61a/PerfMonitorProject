082a5dc6acaed0231683d9c8b1c48502e4f84165
Martin <marb61a@gmail.com>
2020-08-05 20:20:46 +0100

    added initial nodeClient index.js

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,1 +1,21 @@
-// Captures local perfformance data
+// Captures local perfformance data + sends to the socket.io server
+const os = require('os');
+
+// What OS is being used
+const osType = os.type();
+
+// How long the system has been up
+const upTime = os.uptime();
+
+// How much memory is being used and is available on the system
+const freeMem = os.freemem();
+const totalMem = os.totalmem();
+
+const usedMem = totalMem - freeMem;
+const memUsage = Math.floor(usedMem / totalMem * 100);
+
+console.log(osType);
+console.log(upTime);
+console.log(freeMem);
+console.log(totalMem);
+console.log(memUsage);
